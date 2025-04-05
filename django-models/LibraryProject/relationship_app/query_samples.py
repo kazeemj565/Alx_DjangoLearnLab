12c4ab5ea1f31ba9eb886c1a06e7445ba32938fd
kazeem jamiu <kazeemj565@gmail.com>
2025-04-05 22:59:02 +0100

    refactor: modify query_samples.py file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,8 +5,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query 1: Get all books by a specific author (e.g., "George Orwell")
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
-
+    books = Book.objects.filter(author=author)
+    return books
 # Query 2: List all books in a specific library (e.g., "Central Library")
 def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
@@ -16,3 +16,6 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
+
+
+
