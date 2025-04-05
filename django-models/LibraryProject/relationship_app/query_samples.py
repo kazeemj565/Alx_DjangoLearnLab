ad03a70690bd79ab672608c4573124b0f09e7fc9
kazeem jamiu <kazeemj565@gmail.com>
2025-04-05 23:01:44 +0100

    refactor: modify query_samples.py file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -15,7 +15,7 @@ def get_books_in_library(library_name):
 # Query 3: Retrieve the librarian for a specific library (e.g., "Central Library")
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
-
+    librarian = Librarian.objects.get(library=library)
+    return librarian
 
 
