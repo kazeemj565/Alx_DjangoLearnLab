7fa8e29c6b6bdb4ae1653493e72c7c87f4e3631f
kazeem jamiu <kazeemj565@gmail.com>
2025-04-06 22:19:01 +0100

    refactor: modify relationship_app/views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -19,8 +19,7 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Library, Book
-from django.views.generic import DetailView
-
+from django.views.generic.detail import DetailView
 # Function-based view to list all books.
 def list_books(request):
     books = Book.objects.all()
