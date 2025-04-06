86af822a27477d0d633642bd4ff40d39b0083527
kazeem jamiu <kazeemj565@gmail.com>
2025-04-06 22:15:35 +0100

    refactor: modify relationship_app/views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
+# from django.shortcuts import render, get_object_or_404
+# from .models import Book, Library
+# from django.views.generic import DetailView
+
+# # Function-based view to list all books.
+# def list_books(request):
+#     books = Book.objects.all()
+#     return render(request, 'list_books.html', {'books': books})
+
+
+# class LibraryDetailView(DetailView):
+#     model = Library
+#     template_name = 'library_detail.html'
+#     context_object_name = 'library'
+
+
+
+# relationship_app/views.py
+
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Library
 from django.views.generic import DetailView
@@ -5,10 +24,11 @@ from django.views.generic import DetailView
 # Function-based view to list all books.
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # Explicitly reference the template within the relationship_app folder.
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-
+# Class-based view for library details.
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
