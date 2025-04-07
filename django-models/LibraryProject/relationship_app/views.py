b35e396f320f3189d16d95de6a766ad679bfb7e9
kazeem jamiu <kazeemj565@gmail.com>
2025-04-07 11:10:33 +0100

    chore: rearrange the import modules

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,29 +1,15 @@
-# from django.shortcuts import render, get_object_or_404
-# from .models import Book, Library
-# from django.views.generic import DetailView
-
-# # Function-based view to list all books.
-# def list_books(request):
-#     books = Book.objects.all()
-#     return render(request, 'list_books.html', {'books': books})
-
-
-# class LibraryDetailView(DetailView):
-#     model = Library
-#     template_name = 'library_detail.html'
-#     context_object_name = 'library'
-
-
-
 # relationship_app/views.py
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+
 # Function-based view to list all books.
 def list_books(request):
     books = Book.objects.all()
-    # Explicitly reference the template within the relationship_app folder.
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view for library details.
@@ -31,3 +17,15 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the new user.
+            return redirect('list_books')  # Redirect to a view of your choice.
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
