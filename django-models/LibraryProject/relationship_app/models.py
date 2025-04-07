c5678274e21d664e3e2918d385990086e80068b4
kazeem jamiu <kazeemj565@gmail.com>
2025-04-07 12:56:16 +0100

    Implementing Custom Permissions in the django-models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -22,12 +22,24 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+from django.db import models
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    library = models.ForeignKey("Library", on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
+
 
 class Library(models.Model):
     name = models.CharField(max_length=100)
@@ -43,3 +55,20 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+    library = models.ForeignKey("Library", on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
