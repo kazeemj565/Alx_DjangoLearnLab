904b901652a4706fcbaa3c16a0d09b33b84c6b3e
kazeem jamiu <kazeemj565@gmail.com>
2025-04-08 23:00:29 +0100

    feat: add ExampleForm class to forms.py file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year', 'library']
+
+
+# Add this form to satisfy the requirement
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
