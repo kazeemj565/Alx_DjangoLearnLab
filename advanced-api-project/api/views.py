ca62a756e2e4d5353d4ec7c1164fb683c73c4cdc
kazeem jamiu <kazeemj565@gmail.com>
2025-04-09 13:59:46 +0100

    feat: importing rest_framework from django_filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django_filters import rest_framework
 # Create your views here.
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, AllowAny
