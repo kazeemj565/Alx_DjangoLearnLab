736cfea08960b5cd85271edbf82445d80a968bef
kazeem jamiu <kazeemj565@gmail.com>
2025-04-10 08:58:02 +0100

    fix: change the order of urlpatterns in  the blog/urls.py file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('', PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/new/', PostCreateView.as_view(), name='post-create'),
+
 ]
 
